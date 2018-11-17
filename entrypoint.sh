2e722831f750396ffe7873e9899f4e6a33bd1f5f
Andrzej Mazurowski <kontakt@sdrone.pl>
2018-11-17 19:47:13 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/container
+cd /steam
 sleep 1
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
@@ -8,12 +8,12 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 if [ ! -z ${SRCDS_APPID} ]; then
     if [ ! -z ${SRCDS_BETAID} ]; then
         if [ ! -z ${SRCDS_BETAPASS} ]; then
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
+            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /steam/App${SRCDS_APPID} +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
         else
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} +quit
+            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /steam/App${SRCDS_APPID} +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} +quit
         fi
     else
-        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
+        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /steam/App${SRCDS_APPID} +app_update ${SRCDS_APPID} +quit
     fi
 fi
 
